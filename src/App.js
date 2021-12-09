ef078ea03fdcf6e65992ab310f354b8201cdee08
priyankabagave <bagavepriyanka14@gmail.com>
2021-12-09 16:00:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav ClassName="navbar navbar-expand-lg navbar-light bg-light">
+        <div ClassName="container-fluid">
+          <a ClassName="navbar-brand" href="#">Navbar</a>
+          <button ClassName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span ClassName="navbar-toggler-icon"></span>
+          </button>
+          <div ClassName="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul ClassName="navbar-nav me-auto mb-2 mb-lg-0">
+              <li ClassName="nav-item">
+                <a ClassName="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li ClassName="nav-item">
+                <a ClassName="nav-link" href="#">Link</a>
+              </li>
+              <li ClassName="nav-item dropdown">
+                <a ClassName="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul ClassName
+                  ="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a ClassName
+                    ="dropdown-item" href="#">Action</a></li>
+                  <li><a ClassName
+                    ="dropdown-item" href="#">Another action</a></li>
+                  <li><hr ClassName
+                    ="dropdown-divider" /></li>
+                  <li><a ClassName
+                    ="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li ClassName="nav-item">
+                <a ClassName="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form ClassName="d-flex">
+              <input ClassName="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button ClassName="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
